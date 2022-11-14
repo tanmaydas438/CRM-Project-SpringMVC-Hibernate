0386738ad665ab878ef4d133012161f2bb7765c8
Tanmay Das <tanmaydas438@gmail.com>
2022-11-14 09:14:19 +0530

    customer controller

--- src/com/iiitb/crm/controller/CustomerController.java
+++ src/com/iiitb/crm/controller/CustomerController.java
@@ -1,16 +1,27 @@
 package com.iiitb.crm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.iiitb.crm.dao.CustomerDAO;
+import com.iiitb.crm.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
 
 	@RequestMapping("/list")
 	public String listCustomer(Model model)
 	{
+		List<Customer> customers=customerDAO.getCustomers();
+		model.addAttribute("customers", customers);
 		return "list-customers";
 	}
 }
